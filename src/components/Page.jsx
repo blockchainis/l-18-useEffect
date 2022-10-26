da36bcd3330e0de6b0c1ba187f5e49ec4bf1e762
hoon2 <sanghoon.engineer@gmail.com>
2022-10-26 15:46:52 +0900

    최종 완료 코드

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,6 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function Page() {
   const [count, setCount] = useState(0);
+  console.log("렌더링", count);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("effect 실행!", count);
+    //count 0 => count 1
+    return () => {
+      console.log("effect clean up", count);
+    };
+  }, [count]);
+
   return (
     <>
       <p>안녕하세요</p>
